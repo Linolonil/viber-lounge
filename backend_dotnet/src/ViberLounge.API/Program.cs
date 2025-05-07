5a65f1d1187fc87580321ff853c6f56fff1b3bee
ClaudioMarzo <claudio.m.santarem.16@gmail.com>
2025-05-07 13:51:31 -0400

    feat: adicionando rota heath e ajuste para leitura do DbContext

--- backend_dotnet/src/ViberLounge.API/Program.cs
+++ backend_dotnet/src/ViberLounge.API/Program.cs
@@ -1,22 +1,33 @@
-using ViberLounge.API.Extensions;
-using ViberLounge.Infrastructure.Data;
+using HealthChecks.UI.Client;
+using Microsoft.EntityFrameworkCore;
+using ViberLounge.Infrastructure.Context;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
+string ENVIRONMENT = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+string connectionsStrings = ENVIRONMENT == "Development" ? "ConnectionStrings:DefaultConnection" : "ConnectionStrings:ProductionConnection";
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAuthorization();
-builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
 
+configDataBase(builder);
 var app = builder.Build();
 
-await using (var scope = app.Services.CreateAsyncScope())
+// await using (var scope = app.Services.CreateAsyncScope())
+// {
+//     await scope.ServiceProvider.InitializeDatabaseAsync();
+// }
+void configDataBase(WebApplicationBuilder serviceProvider)
 {
-    await scope.ServiceProvider.InitializeDatabaseAsync();
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(
+        builder.Configuration[connectionsStrings],
+        options => options.SetPostgresVersion(new Version(15, 0, 0))
+    ));
 }
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -28,4 +39,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
-await app.RunAsync();
+app.MapHealthChecks("/health", new HealthCheckOptions{ Predicate = _ => true, ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse});
+
+app.Run();
