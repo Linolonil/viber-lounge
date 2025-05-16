03f711c4d4133fc13d929629cb2ac796a07bfcbc
ClaudioMarzo <claudio.m.santarem.16@gmail.com>
2025-05-15 21:11:04 -0400

    fix: ajustando migration e path de wwwroot

---
+++ backend_dotnet/src/ViberLounge.Infrastructure/Migrations/20250515155317_AlterarStatusParaCancelado.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ViberLounge.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterarStatusParaCancelado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Vendas");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Cancelado",
+                table: "Vendas",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cancelado",
+                table: "Vendas");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Vendas",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
